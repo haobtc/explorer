52f27154ab54bb05cb73c938e73d3260c05b6c49
Zeng Ke <superisaac.ke@gmail.com>
2014-09-16 12:10:33 +0800

    Added js script to reset tip

---
+++ scripts/reset_tip.js
@@ -0,0 +1,17 @@
+var block_height = 370880;
+
+db.block.find({height: {$gt: block_height}}).forEach(function(b) {
+  db.tx.remove({bhash: b.hash});
+});
+print('removed adv txes');
+
+db.block.remove({height: {$gt: block_height}});
+
+var maxb = db.block.find({isMain: true}).sort({height: -1}).limit(1)[0];
+print(maxb);
+if(maxb) {
+  db.var.update({key: 'tip'}, {$set: {blockHash: maxb.hash}});
+}
+db.archive.remove();
+db.mempool.remove();
+
