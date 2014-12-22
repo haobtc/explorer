1c24704d86f61420282f86ca5e9dae815304d09c
Zeng Ke <superisaac.ke@gamil.com>
2014-12-22 18:24:44 +0800

    added block counter tx

---
+++ mongoscripts/blockcnttx.js
@@ -0,0 +1,8 @@
+
+db.block.find().sort({$natural: -1}).skip(0).limit(10000).forEach(function(block) {
+  if(!block.isMain) return;
+  var cnt = db.tx.find({bhs: block.hash}).count();
+  if(block.cntTxes != cnt){
+    print(block.hash.hex());
+  }
+});
