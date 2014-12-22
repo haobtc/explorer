b3b1c421b9f25eaaa29213449a2d924b08947127
Zeng Ke <superisaac.ke@gmail.com>
2014-12-22 18:39:57 +0800

    refactor addTxes

--- mongoscripts/blockcnttx.js
+++ mongoscripts/blockcnttx.js
@@ -2,6 +2,7 @@
 db.block.find().sort({$natural: -1}).skip(0).limit(10000).forEach(function(block) {
   if(!block.isMain) return;
   var cnt = db.tx.find({bhs: block.hash}).count();
+  print('cnt', cnt);
   if(block.cntTxes != cnt){
     print(block.hash.hex());
   }
