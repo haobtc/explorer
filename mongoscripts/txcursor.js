8b720be7164aa8c3197043552bfa7e10606bbfa9
Zeng Ke <superisaac.ke@gmail.com>
2015-03-05 15:03:34 +0800

    Online fix

--- mongoscripts/txcursor.js
+++ mongoscripts/txcursor.js
@@ -1,11 +1,16 @@
 'use strict';
 
-function fillInputs(tx) {
+function TxCursor(url) {
+  this.db = connect(url);
+}
+
+TxCursor.prototype.fillInputs = function(tx) {
+  var self = this;
   var updateSet = {};
   var changed = false;
   tx.vin.forEach(function(input, index) {
     if(!input.addrs && input.hash) {
-      var inputTx = db.tx.findOne({hash: input.hash});
+      var inputTx = self.db.tx.findOne({hash: input.hash});
       if(inputTx) {
 	input.addrs = inputTx.vout[input.n].addrs;
 	input.v = inputTx.vout[input.n].v;
@@ -14,26 +19,28 @@ function fillInputs(tx) {
 	changed = true;
       } else {
 	var message = 'Cannot find input of ' + input.hash.hex() + '#' + input.n + ' by ' + tx.hash.hex() + '.vin.' + index;
-	db.errormsg.insert({message: message});
+	self.db.errormsg.insert({message: message});
       }
     }
   });
   if(changed) {
-    db.tx.update({hash: tx.hash}, {$set: updateSet});
+    this.db.tx.update({hash: tx.hash}, {$set: updateSet});
   }
-}
+};
 
-function updateInputSpt(tx) {
+TxCursor.prototype.updateInputSpt = function(tx) {
+  var self = this;
   tx.vin.forEach(function(input) {
     if(input.hash) {
       var updateSet = {};
       updateSet['vout.' + input.n + '.spt'] = 1;
-      db.tx.update({hash: input.hash}, {$set: updateSet});
+      self.db.tx.update({hash: input.hash}, {$set: updateSet});
     }
   });
-}
+};
 
-function updateAddress(tx) {
+TxCursor.prototype.updateAddress = function(tx) {
+  var self = this;
   var addrAmount = {};
   function loadAddr(addr) {
     if(!addrAmount[addr]) {
@@ -47,7 +54,7 @@ function updateAddress(tx) {
       loadAddr(addr);
       var amount = NumberLong(input.v);
       addrAmount[addr] -= amount;
-      db.address.update({_id: addr}, {$inc: {'bal': -amount}}, {upsert: true});
+      self.db.address.update({_id: addr}, {$inc: {'bal': -amount}}, {upsert: true});
     }
   });
   tx.vout.forEach(function(output) {
@@ -56,58 +63,62 @@ function updateAddress(tx) {
       loadAddr(addr);
       var amount = NumberLong(output.v);
       addrAmount[addr] += amount;
-      db.address.update({_id: addr}, 
-			{$inc: {'bal': amount, 'recv': amount}}, {upsert: true});
+      self.db.address.update({_id: addr}, 
+			     {$inc: {'bal': amount, 'recv': amount}}, {upsert: true});
     }
   });
 
   for(var addr in addrAmount) {
     if(addrAmount.hasOwnProperty(addr)) {
-      var a = db.address.findOne({_id: addr});
+      var a = self.db.address.findOne({_id: addr});
       var txs = (a&&a.txs)?a.txs:[];
       txs.push({t: tx.hash,v:addrAmount[addr]});
       while(txs.length > 50) {
 	txs.shift();
       }
-      db.address.update({_id: addr}, {$inc: {'c': 1}, $set: {txs: txs}});
+      self.db.address.update({_id: addr}, {$inc: {'c': 1}, $set: {txs: txs}});
     }
   }
-}
+};
 
-function cursorNext() {
-  db.runCommand('beginTransaction');
+TxCursor.prototype.next = function() {
+  this.db.runCommand('beginTransaction');
 
   // Get tx
-  var v = db.var.findOne({key: 'txcursor'});
+  var v = this.db.var.findOne({key: 'txcursor'});
   var tx;
   if(!v) {
-    tx = db.tx.findOne();
+    tx = this.db.tx.findOne();
   } else {
-    tx = db.tx.find({_id: {$gt: v.objid}}).sort({_id: 1}).limit(1)[0];
+    tx = this.db.tx.find({_id: {$gt: v.objid}}).sort({_id: 1}).limit(1)[0];
   }
 
   if(!tx) {
-    db.runCommand('rollbackTransaction');
-    sleep(1000);
-    quit();
+    this.db.runCommand('rollbackTransaction');
     return false;
   }
-  //print(tx.hash.hex());
-  // fill the input addrs and v of tx
-  fillInputs(tx);
+  this.fillInputs(tx);
 
-  updateInputSpt(tx);
-  updateAddress(tx);  
+  this.updateInputSpt(tx);
+  this.updateAddress(tx);  
 
-  db.var.update({key: 'txcursor'},  {$set: {objid: tx._id}}, {upsert: true});
-  db.runCommand('commitTransaction');
+  this.db.var.update({key: 'txcursor'},  {$set: {objid: tx._id}}, {upsert: true});
+  this.db.runCommand('commitTransaction');
   return true;
-}
+};
 
-print(new Date(), 'txcount starts');
-for(var i=0; i<10000; i++) {
-/*  if(i % 1000 == 0){
-    print(i);
-  } */
-  cursorNext();
-}
+// TxCursor.prototype.blockJob = function() {
+//   this.db.runCommand('beginTransaction');
+//   var v = this.db.var.findOne({key: 'blockCleanup'});
+  
+//   this.db.runCommand('commitTransaction');
+// };
+
+
+['bitcoin', 'dogecoin', 'litecoin', 'darkcoin'].forEach(function(network) {
+  var txCursor = new TxCursor('localhost:27017/blocks_' + network);
+  for(var i=0; i<1000; i++) {
+    var r = txCursor.next();
+    print('r', r);
+  }
+});
